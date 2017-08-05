9637861b4fb11ee399c383aa5e1e940ba5c39883
sho kisaragi <sho@oshiire.to>
2017-08-05 22:26:42 +0900

    メソッドの細分化

--- lib/reins.rb
+++ lib/reins.rb
@@ -13,47 +13,62 @@ require "socket"
 require "ipaddr"
 
 module Reins
+  class << self
+    def run_server(port)
+      Reins.logger.info("Reins #{VERSION} を #{Reins.port} で起動します")
+      @server = TCPServer.new(port)
+    rescue => e
+      Reins.logger.error("Reins が起動できませんでした: #{e}")
+      exit
+    end
+
+    def define_value(client)
+      @addr = IPAddr.new(client.peeraddr[3]).native.to_s
+      @keycode, @command, @options = client.gets.chomp.split
+      Reins.logger.debug("addr = #{@addr}, keycode = #{@keycode}, command = #{@command}, options = #{@options}")
+    end
+
+    def run_auth
+      Reins.logger.debug("#{@addr} : 認証を行います")
+      if (key = Reins.auth_service.authenticate_key(@keycode, @addr))
+        if key == true
+          Reins.regist_host.read_hostkeys[@addr]
+        else
+          Reins.regist_host.create(@addr, key) ? key : "false"
+        end
+      else
+        false
+      end
+    end
+
+    def run_command
+      if Reins.auth_service.varid?(@keycode) == @addr
+        Reins::Dispatch.new(@addr, @keycode).command(@command, @options)
+      else
+        "false"
+      end
+    end
+
+    def exit_server
+      Reins.logger.info("Reins #{VERSION} を終了します")
+      Reins.regist_host.store
+      @server.close
+      exit
+    end
+  end
+
   def start
-    server = TCPServer.new(Reins.port)
-    Reins.logger.info("Reins #{VERSION} を #{Reins.port} で起動しました")
+    run_server(Reins.port)
 
     loop do
       begin
-        Thread.start(server.accept) do |client|
-          addr = IPAddr.new(client.peeraddr[3]).native.to_s
-          keycode, command, options = client.gets.chomp.split
-          Reins.logger.debug("addr = #{addr}, keycode = #{keycode}, command = #{command}, options = #{options}")
-
-          if command == 'auth'
-            Reins.logger.debug("#{addr} : 認証を行います")
-            if (@keycode = Reins.auth_service.authenticate_key(keycode, addr))
-              if @keycode == true
-                client.puts Reins.regist_host.read_hostkeys[addr]
-              else
-                Reins.logger.debug("取得したキーコード : #{@keycode}")
-                client.puts Reins.regist_host.create(addr, @keycode) ? @keycode : "NG: ホスト登録失敗"
-              end
-            else
-              client.puts "NG: 認証ミス"
-            end
-
-          elsif Reins.auth_service.varid?(keycode) == addr
-            Reins.logger.debug("#{command} : 実行します")
-            @host = Reins::Dispatch.new(addr, keycode)
-
-            client.puts @host.command(command, options)
-          else
-            Reins.logger.warn("#{command} : 実行できませんでした")
-            client.puts "NG: コマンドが実行できませんでした"
-          end
-
+        Thread.start(@server.accept) do |client|
+          define_value(client)
+          client.puts @command == 'auth' ? run_auth : run_command
           client.close
         end
-      rescue Interrupt => e
-        Reins.logger.info(e.to_s)
-        Reins.regist_host.store
-        server.close
-        exit
+      rescue Interrupt
+        exit_server
       end
     end
   end
