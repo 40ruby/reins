f7f2250fe174a76da01d6554942c7f3eec714a97
sho kisaragi <sho@oshiire.to>
2017-10-09 08:56:39 +0900

    Added Sequence Diagrams

--- lib/reins.rb
+++ lib/reins.rb
@@ -14,20 +14,48 @@ require "ipaddr"
 
 module Reins
   class << self
+    # 指定されたポートでサーバを起動する
+    # == パラメータ
+    # port:: 割り当てるポート番号
+    # == 返り値
+    # Exception:: サーバが起動できなかった場合、例外を発生させ起動させない
+    # TCPSocket:: 正常に起動した場合、Socket オブジェクトを返す
     def run_server(port)
-      Reins.logger.info("Reins #{VERSION} を #{Reins.port} で起動します")
-      @server = TCPServer.new(port)
+      Reins.logger.info("Reins #{VERSION} を #{port} で起動します")
+      TCPServer.new(port)
     rescue => e
       Reins.logger.error("Reins が起動できませんでした: #{e}")
+      puts(e.to_s)
       exit
     end
 
-    def define_value(client)
-      @addr = IPAddr.new(client.peeraddr[3]).native.to_s
+    # 起動されているサーバを停止する
+    # == パラメータ
+    # server:: 起動中のサーバの Socket
+    # == 返り値
+    # 特になし
+    def exit_server(server)
+      Reins.logger.info("Reins #{VERSION} を終了します")
+      Reins.regist_host.store
+      server.close
+      exit
+    end
+  end
+
+  class Clients
+    attr_reader :command
+    def initialize(client)
       @keycode, @command, @options = client.gets.chomp.split
+      @addr = IPAddr.new(client.peeraddr[3]).native.to_s
       Reins.logger.debug("addr = #{@addr}, keycode = #{@keycode}, command = #{@command}, options = #{@options}")
     end
 
+    # 認証処理を行う
+    # == パラメータ
+    # 特になし
+    # == 返り値
+    # key:: 認証が成功した場合は接続用の認証キーを返す
+    # false:: 認証が失敗した時は "false" 文字列を返す
     def run_auth
       Reins.logger.debug("#{@addr} : 認証を行います")
       if (key = Reins.auth_service.authenticate_key(@keycode, @addr))
@@ -37,10 +65,16 @@ module Reins
           Reins.regist_host.create(@addr, key) ? key : "false"
         end
       else
-        false
+        "false"
       end
     end
 
+    # サーバで実行されるコマンドを受け渡す
+    # == パラメータ
+    # 特になし
+    # == 返り値
+    # false:: 実行できなければ "false" 文字列を返す
+    # false以外:: 実行された結果を、改行を含む文字列で返す
     def run_command
       if Reins.auth_service.varid?(@keycode) == @addr
         Reins::Dispatch.new(@addr, @keycode).command(@command, @options)
@@ -48,27 +82,20 @@ module Reins
         "false"
       end
     end
-
-    def exit_server
-      Reins.logger.info("Reins #{VERSION} を終了します")
-      Reins.regist_host.store
-      @server.close
-      exit
-    end
   end
 
   def start
-    run_server(Reins.port)
+    server = run_server(Reins.port)
 
     loop do
       begin
-        Thread.start(@server.accept) do |client|
-          define_value(client)
-          client.puts @command == 'auth' ? run_auth : run_command
-          client.close
+        Thread.start(server.accept) do |c|
+          client = Reins::Clients.new(c)
+          c.puts client.command == 'auth' ? client.run_auth : client.run_command
+          c.close
         end
       rescue Interrupt
-        exit_server
+        exit_server(server)
       end
     end
   end
